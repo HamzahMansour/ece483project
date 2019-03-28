b876fe5843960a2080e7c07cf681ce69f1762b06
joshuacwnewton <joshuacwnewton@gmail.com>
2019-03-28 16:28:53 -0700

    Wrote script to test motion_estimation.m in isolation from full encoder.

---
+++ tests/motion_estimation_test.m
@@ -0,0 +1,28 @@
+function motion_estimation_test
+    clear all; close all;
+
+    % Add encoder folder to path
+    currentDir = pwd;
+    idcs   = strfind(pwd,filesep);
+    parentDir = currentDir(1:idcs(end)-1);
+    addpath(strcat(parentDir,'\lossy_encoder'));
+    
+    % Choose anchor/current image, check validity
+    [fileA,pathA] = uigetfile('*.tif');
+    if ~isequal(fileA,0)
+        % Choose target/reference image, check validity
+        [fileT,pathT] = uigetfile('*.tif');
+        if ~isequal(fileT,0)
+            % Load test frames into MATLAB
+            anchorFrame = imread(strcat(pathA,fileA));
+            targetFrame = imread(strcat(pathT,fileT));
+
+            % Execute motion estimation algorithm
+            [errorFrame, displacementV, displacementH] = ...
+            motion_estimation(double(targetFrame), double(anchorFrame));
+        end
+    end
+    
+    % Display error image (complemented for visual inspection purposes)
+    imshow(imcomplement(uint8(errorFrame)));
+end
--- tests/test.m
+++
@@ -1,1 +0,0 @@
-% Files in this folder will test lossy_encoder.m
