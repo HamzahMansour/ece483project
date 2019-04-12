cb2dc0e807ebffcf18ed9df396ad62283f63a775
joshuacwnewton <joshuacwnewton@gmail.com>
2019-04-12 16:34:37 -0700

    Wrote 2 new BMA options for lossy_encoder.

--- lossy_encoder/motion_estimation.m
+++ lossy_encoder/motion_estimation.m
@@ -1,5 +1,5 @@
 function [predictedFrame, displacementV, displacementH] = motion_estimation ...
-    (targetFrame, anchorFrame, searchRange, mbSize, BMA)
+    (targetFrame, anchorFrame, BMA)
     % Input arguments:
     %   - targetFrame: Stored previous frame. This frame will be searched for
     %                  matching blocks to create predicted frame.
@@ -7,33 +7,25 @@ function [predictedFrame, displacementV, displacementH] = motion_estimation ...
     %                  blocks from target frame.
     %   - blockSize: Dimensions of NxN macroblock.
     %   - BMA: Choice for block-matching algorithm. (EBMA, 3SS, 2DLS)
-    %   - searchRange: EBMA parameter.
     %
     % Output arguments:
     %   - errorFrame: Difference between anchorFrame and predicted frame.
     %   - displacementV: Vertical component of displacement vectors.
     %   - displacementH: Horizontal component of displacement vectors.
-    
+
     % Setting a default values for parameters if not supplied
-    if (~exist('searchRange', 'var'))
-        searchRange = 5;
-    end
-    
-    if (~exist('blockSize', 'var'))
-        mbSize = 16;
-    end
-    
     if (~exist('BMA', 'var'))
         BMA = 1;
     end
-    
+
     % Precalculate frame and block properties
+    mbSize = 16;
     [frameHeight,frameWidth] = size(anchorFrame);
     vBlocks = frameHeight/mbSize;
     hBlocks = frameWidth/mbSize;
     maxHeight = frameHeight-(mbSize-1);
     maxWidth = frameWidth-(mbSize-1);
-    windowSize = (searchRange-1)/2;
+    S = 7; % Implies a 15x15 search window
 
     % Preallocate arrays for displacement vectors, predicted frame
     displacementV = zeros(vBlocks,hBlocks);
@@ -54,23 +46,23 @@ function [predictedFrame, displacementV, displacementH] = motion_estimation ...
                      displacementV(ceil(pAnchorV/mbSize)),       ...
                      displacementH(ceil(pAnchorH/mbSize))] =     ...
                     EBMA(targetFrame, anchorBlock, pAnchorV, pAnchorH, ...
-                         windowSize, mbSize, maxHeight, maxWidth);
+                         S, mbSize, maxHeight, maxWidth);
                 case 2
                     % Three-Step Search for best-matching block and MV
                     [predictedFrame(pAnchorV:pAnchorV+mbSize-1,  ...
                                     pAnchorH:pAnchorH+mbSize-1), ...
                      displacementV(ceil(pAnchorV/mbSize)),       ...
                      displacementH(ceil(pAnchorH/mbSize))] =     ...
-                    SS_3(targetFrame, anchorBlock, pAnchorV, pAnchorH, ...
-                         windowSize, mbSize, maxHeight, maxWidth);
+                    TSS(targetFrame, anchorBlock, pAnchorV, pAnchorH, ...
+                        S, mbSize, maxHeight, maxWidth);
                 case 3
                     % 2D Log Search for best-matching block and MV
                     [predictedFrame(pAnchorV:pAnchorV+mbSize-1,  ...
                                     pAnchorH:pAnchorH+mbSize-1), ...
                      displacementV(ceil(pAnchorV/mbSize)),       ...
                      displacementH(ceil(pAnchorH/mbSize))] =     ...
-                    LS_2D(targetFrame, anchorBlock, pAnchorV, pAnchorH, ...
-                          windowSize, mbSize, maxHeight, maxWidth);
+                    TDLS(targetFrame, anchorBlock, pAnchorV, pAnchorH, ...
+                         S, mbSize, maxHeight, maxWidth);
                 otherwise
                     print("Not a valid choice for BMA");
             end
@@ -81,7 +73,7 @@ end
 
 function [predictedBlock, displacementV, displacementH] = EBMA ...
     (targetFrame, anchorBlock, pAnchorV, pAnchorH, ...
-     windowSize, mbSize, maxHeight, maxWidth)
+     S, mbSize, maxHeight, maxWidth)
     % Input arguments:
     %   - targetFrame: Frame to search through for best match block.
     %   - anchorBlock: Block which must be matched.
@@ -92,19 +84,15 @@ function [predictedBlock, displacementV, displacementH] = EBMA ...
     %   - predictedBlock: Best match for anchorBlock found in targetFrame.
     %   - displacementV: Vertical component of motion vector.
     %   - displacementH: Horizontal component of motion vector.
-    
+
     % Reset lowest error condition
     lowestError = Inf;
 
     % Iterate through search window exhaustively
-    for pSearchV = -windowSize:windowSize
-        for pSearchH = -windowSize:windowSize
+    for pTargetV = (pAnchorV-S):(pAnchorV+S)
+        for pTargetH = (pAnchorH-S):(pAnchorH+S)
 
-            % Convert relative pointer to absolute pointer
-            pTargetV = pAnchorV + pSearchV;
-            pTargetH = pAnchorH + pSearchH;
-
-            % Ensuring target frame pointers are within boundaries
+            % Ensuring target frame pointers are within image boundary
             if (pTargetV >= 1 && pTargetV <= maxHeight && ...
                 pTargetH >= 1 && pTargetH <= maxWidth)
 
@@ -123,22 +111,147 @@ function [predictedBlock, displacementV, displacementH] = EBMA ...
 
                     % Update best match block and its relative location
                     predictedBlock = targetBlock;
-                    displacementV = pSearchV;
-                    displacementH = pSearchH;
+                    displacementV = pTargetV - pAnchorV;
+                    displacementH = pTargetH - pAnchorH;
                 end
             end
         end
     end
-    
-    function [predictedBlock, displacementV, displacementH] = SS_3 ...
-    (targetFrame, anchorBlock, pAnchorV, pAnchorH, ...
-     windowSize, mbSize, maxHeight, maxWidth)
- 
-    end
+end
 
-    function [predictedBlock, displacementV, displacementH] = LS_2D ...
+function [predictedBlock, displacementV, displacementH] = TSS ...
     (targetFrame, anchorBlock, pAnchorV, pAnchorH, ...
-     windowSize, mbSize, maxHeight, maxWidth)
- 
+     S, mbSize, maxHeight, maxWidth)
+    % Input arguments:
+    %   - targetFrame: Frame to search through for best match block.
+    %   - anchorBlock: Block which must be matched.
+    %   - pAnchorV: Pointer indicating vertical index of anchorBlock.
+    %   - pAnchorH: Pointer indicating horizontal index of anchorBlock.
+    %   
+    % Output arguments:
+    %   - predictedBlock: Best match for anchorBlock found in targetFrame.
+    %   - displacementV: Vertical component of motion vector.
+    %   - displacementH: Horizontal component of motion vector.
+    
+    % Initialize using target block from anchor block location. (Doing 
+    % outside loop as center location not checked in recursive steps.)
+    targetBlock = targetFrame(pAnchorV:pAnchorV+mbSize-1, ...
+                              pAnchorH:pAnchorH+mbSize-1);
+    differenceBlock = targetBlock - anchorBlock;
+    lowestError = sum(abs(differenceBlock),'all');      
+    predictedBlock = targetBlock;
+    displacementV = pAnchorV;
+    displacementH = pAnchorH;
+    
+    % List of neighbour coordinates to search through 
+    search = [ 1  1;  1  0;  1 -1; ...
+               0  1;         0 -1;  ...
+              -1  1; -1  0; -1 -1];
+          
+    while S > 1
+        S = round(S/2);  % Halve step size
+        for i = 1:8
+            % Update target pointer with location of one of 8 neighbours
+            pTargetV = pAnchorV + S*search(i,1);
+            pTargetH = pAnchorH + S*search(i,2);
+            
+            % Ensuring target frame pointers are within image boundary
+            if (pTargetV >= 1 && pTargetV <= maxHeight && ...
+                pTargetH >= 1 && pTargetH <= maxWidth)
+
+                % Storing values contained in current target block
+                targetBlock = targetFrame(pTargetV:pTargetV+mbSize-1, ...
+                                          pTargetH:pTargetH+mbSize-1);
+
+                % MAD criterion calculation
+                differenceBlock = targetBlock - anchorBlock;
+                errorTerm = sum(abs(differenceBlock),'all');
+                
+                % Check to see if lowest error in search window
+                if errorTerm < lowestError
+                    % Updating error condition value
+                    lowestError = errorTerm;
+
+                    % Update best match block and its relative location
+                    predictedBlock = targetBlock;
+                    displacementV = pTargetV - pAnchorV;
+                    displacementH = pTargetH - pAnchorH;
+                end
+            end
+        end  
+        % Update center location for next step
+        pAnchorV = displacementV;
+        pAnchorH = displacementH;
     end
 end
+
+function [predictedBlock, displacementV, displacementH] = TDLS ...
+    (targetFrame, anchorBlock, pAnchorV, pAnchorH, ...
+     S, mbSize, maxHeight, maxWidth)
+    % Input arguments:
+    %   - targetFrame: Frame to search through for best match block.
+    %   - anchorBlock: Block which must be matched.
+    %   - pAnchorV: Pointer indicating vertical index of anchorBlock.
+    %   - pAnchorH: Pointer indicating horizontal index of anchorBlock.
+    %   
+    % Output arguments:
+    %   - predictedBlock: Best match for anchorBlock found in targetFrame.
+    %   - displacementV: Vertical component of motion vector.
+    %   - displacementH: Horizontal component of motion vector.
+    
+    % Initialize using target block from anchor block location. (Doing 
+    % outside loop as center location not checked in recursive steps.)
+    targetBlock = targetFrame(pAnchorV:pAnchorV+mbSize-1, ...
+                              pAnchorH:pAnchorH+mbSize-1);
+    differenceBlock = targetBlock - anchorBlock;
+    lowestError = sum(abs(differenceBlock),'all');      
+    predictedBlock = targetBlock;
+    displacementV = pAnchorV;
+    displacementH = pAnchorH;
+    
+    % List of neighbour coordinates to search through 
+    search = [        1  0;         ...
+               0  1;         0 -1;  ...
+                     -1  0;      ];
+          
+    while S > 1
+        for i = 1:4
+            % Update target pointer with location of one of 4 neighbours
+            pTargetV = pAnchorV + S*search(i,1);
+            pTargetH = pAnchorH + S*search(i,2);
+            
+            % Ensuring target frame pointers are within image boundary
+            if (pTargetV >= 1 && pTargetV <= maxHeight && ...
+                pTargetH >= 1 && pTargetH <= maxWidth)
+
+                % Storing values contained in current target block
+                targetBlock = targetFrame(pTargetV:pTargetV+mbSize-1, ...
+                                          pTargetH:pTargetH+mbSize-1);
+
+                % MAD criterion calculation
+                differenceBlock = targetBlock - anchorBlock;
+                errorTerm = sum(abs(differenceBlock),'all');
+                
+                % Check to see if lowest error in search window
+                if errorTerm < lowestError
+                    % Updating error condition value
+                    lowestError = errorTerm;
+
+                    % Update best match block and its relative location
+                    predictedBlock = targetBlock;
+                    displacementV = pTargetV - pAnchorV;
+                    displacementH = pTargetH - pAnchorH;
+                end
+            end
+        end  
+        if (pAnchorV == displacementV)
+            % Halve step size if best location did not change
+            S = round(S/2);  
+        else
+            % Update location for next step
+            pAnchorV = displacementV;
+            pAnchorH = displacementH;
+        end
+    end 
+end
+
