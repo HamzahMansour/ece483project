4803ad2187fee759577b1a4ab8cb74ba3b2c0ba5
HamzahMansour <hamzahbmansour@gmail.com>
2019-04-17 00:29:49 -0700

    lossy encoder outputs SSIM, time, and zero-coefficients, encoder_test modified

--- lossy_encoder/lossy_encoder.m
+++ lossy_encoder/lossy_encoder.m
@@ -1,4 +1,4 @@
-function [quantized_coeff, motion_vectors] = ...
+function [quantized_coeff, motion_vectors, ssim_values, zeros_percentage, times] = ...
     lossy_encoder(input_video, output_video, qf, gop, bma)
 %% Compress raw video using temporal and spatial compression methods.
 % Input arguments:
@@ -21,7 +21,10 @@ function [quantized_coeff, motion_vectors] = ...
     % for full video. Fix later.)
     quantized_coeff = cell(100,1);
     motion_vectors = cell(100,2);
-    
+    ssim_values = zeros(100, 1);
+    zeros_percentage = zeros(100, 1);
+    times = zeros(100, 1);
+
     open(v_out);
     frame_counter = 0;
     % while hasFrame(v_in);
@@ -32,6 +35,7 @@ function [quantized_coeff, motion_vectors] = ...
         current_frame = readFrame(v_in);
         input_frame = double(rgb2gray(current_frame));
 
+        timerVal = tic;
         % I frame
         if frame_counter(mod(frame_counter, gop) == 1)
             % Convert image to quantized DCT coefficients at specified QF
@@ -53,17 +57,23 @@ function [quantized_coeff, motion_vectors] = ...
                 generate_coefficients(error_frame, qf);
             % Convert DCT coefficients to image at specified QF
             error_frame = generate_frame(quantized_coeff{frame_counter,1},qf);
-            % Combine 
+            % Combine
             stored_frame = predicted_frame + error_frame;
-        end        
+        end
+        times(frame_counter) = toc(timerVal);
+
+
+        ssim_values(frame_counter) = ssim(stored_frame, input_frame);
+        zeros_percentage(frame_counter) = nnz(~quantized_coeff{frame_counter, 1})/numel(quantized_coeff{frame_counter, 1});
+
         % Write stored frame to file for viewing results
-        writeVideo(v_out,uint8(stored_frame)); 
+        writeVideo(v_out,uint8(stored_frame));
 
         % Status update
         if (mod(frame_counter, 10) == 0)
             disp([num2str(frame_counter),' frames saved']);
         end
     end
-    
+
     close(v_out);
 end
--- tests/encoder_test.m
+++ tests/encoder_test.m
@@ -1,26 +1,43 @@
 function encoder_test
     clear all; close all;
-    
+
     % Add encoder folder to path
     currentDir = pwd;
     idcs   = strfind(pwd,filesep);
     parentDir = currentDir(1:idcs(end)-1);
     addpath(strcat(parentDir,'\lossy_encoder'));
-    
+
     % Set input parameters
-    [file,path] = uigetfile({'*.avi';'*.*'}, 'Select Raw Video');
-    video_path = strcat(path,file);
+    video_path = strcat(currentDir,'\test_video1.mp4');
     gop = 7;
-    
+
     % Test encoder using three different BMA types
     bmas = ["EBMA", "TSS", "TDLS"];
+
+    % run encoder for each BMA + each QF
+    
+    hold on for i = 1:3
+        [quantized_coeff, motion_vectors, ssim_values, zeros_percentage,
+        times] = ...
+            lossy_encoder(video_path,
+            strcat("output_video_qf50_",bmas(i),".avi"), 50, gop, i);
+        plot(ssim_values);
+    end hold off
+
+    hold on
     for i = 1:3
-        % Testing encoder using three different quantization matrix QFs
-        [quantized_coeff, motion_vectors] = ...
-            lossy_encoder(video_path, strcat("output_video_qf10_",bmas(i),".avi"), 10, gop, i);
-        [quantized_coeff, motion_vectors] = ...
+        [quantized_coeff, motion_vectors, ssim_values, zeros_percentage, times] = ...
             lossy_encoder(video_path, strcat("output_video_qf50_",bmas(i),".avi"), 50, gop, i);
-        [quantized_coeff, motion_vectors] = ...
-            lossy_encoder(video_path, strcat("output_video_qf90_",bmas(i),".avi"), 90, gop, i);
+        plot(zeros_percentage);
     end
+    hold off
+
+    hold on
+    for i = 1:3
+        [quantized_coeff, motion_vectors, ssim_values, zeros_percentage, times] = ...
+            lossy_encoder(video_path, strcat("output_video_qf50_",bmas(i),".avi"), 50, gop, i);
+        plot(times);
+    end
+    hold off
+
 end
